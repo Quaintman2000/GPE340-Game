479dadf1c47ea3ca69c9496d44567fff4d87f9cb
Alec Carter <Quaintman2000@gmail.com>
2021-05-25 18:25:04 -0700

    Worked on it

--- Assets/Pawn.cs
+++ Assets/Pawn.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class Pawn : MonoBehaviour
 {
-    public Animator anim;
+    private Animator anim;
+    [SerializeField] private float speed;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +16,15 @@ public class Pawn : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        anim.SetFloat("Forward", Input.GetAxis("Vertical"));
-        anim.SetFloat("Right", Input.GetAxis("Horizontal"));
+        // Grabs the direction for the player's joystick.
+        Vector3 stickDirection = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        // Max's the magnitude of the vector to 1.
+        stickDirection = Vector3.ClampMagnitude(stickDirection, 1);
+        // Converts the vector to local space.
+        Vector3 animationDirection = transform.InverseTransformDirection(stickDirection);
+
+        // Sets the floats for the animator.
+        anim.SetFloat("Forward", animationDirection.y * speed);
+        anim.SetFloat("Right", animationDirection.x * speed);
     }
 }
