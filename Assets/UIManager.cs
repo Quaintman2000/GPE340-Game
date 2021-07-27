dc3e39a8b970b2019a098b1eb871473f96ed1fe3
Alec Carter <Quaintman2000@gmail.com>
2021-07-26 19:07:55 -0700

    ui thimgs

--- Assets/UIManager.cs
+++ Assets/UIManager.cs
@@ -10,11 +10,33 @@ public class UIManager : MonoBehaviour
     Slider healthSlider;
     // References the player's health.
     public Health playerHealth;
+    public GameObject pauseMenu;
 
+    private void Start()
+    {
+        pauseMenu.SetActive(false);
+    }
     private void Update()
     {
         // Changes the value of the slider based on player's health.
         healthSlider.value = playerHealth.health;
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            pauseMenu.SetActive(true);
+            // GAME MANGER PAUSE.
+        }
+    }
+   
+    public void OnResumeClicked()
+    {
+        pauseMenu.SetActive(false);
+        // GAME MANAGER UPAUSE
+    }
+
+    public void OnQuitClicked()
+    {
+        Application.Quit();
     }
 
 }
